fb51abaeee8d0eed8a185ba317d5aba239ef133b
Megumi Kurihara <pomme.c5.musique@gmail.com>
2021-10-01 16:38:58 +0900

    modified station03

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,28 @@ class Admin::MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @status = [
+      ['上映中', true],
+      ['上映予定', false]
+    ]
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save 
+      flash[:success] = '映画を登録しました'
+        redirect_to 'admin/movies'
+      else
+        flash.now[:danger] = 'エラーが発生しました。'
+        render :new
+      end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'movies' => 'movies#index'
   get 'admin/movies' => 'admin/movies#index'
+  get 'admin/movies/new' => 'admin/movies#new'
+  post 'admin/movies'=> 'admin/movies#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
