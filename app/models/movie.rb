8b172e529631ad6f54a6fe32052b93011b218b8c
Megumi Kurihara <pomme.c5.musique@gmail.com>
2021-10-02 14:18:22 +0900

    modified station03

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 160 } 
+  validates :year, length: { maximum: 45 } 
+  validates :image_url, length: { maximum: 150 } 
 end
